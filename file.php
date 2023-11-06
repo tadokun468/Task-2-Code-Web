ab543bddb13d714a86915366968b0f38e82d20c8
Huỳnh Phạm Thanh Lực <147029581+tadokun468@users.noreply.github.com>
2023-11-06 22:45:17 +0700

    Update file.php

--- file.php
+++ file.php
@@ -6,7 +6,7 @@
 </head>
 <body>
     <form method="post" enctype="multipart/form-data">
-        <!-- Mẫu form cho việc tải lên tệp tin -->
+        <!-- Form cho việc tải lên file -->
         <input type="file" name="fileToUpload"> <!-- Input cho việc chọn tệp tin -->
         <input type="submit" name="upload" value="Upload and Read"> <!-- Nút tải lên -->
     </form>
@@ -16,24 +16,24 @@
         // Kiểm tra xem nút "Upload and Read" đã được nhấn
 
         if(isset($_FILES['fileToUpload'])){
-            // Kiểm tra xem mảng $_FILES có tồn tại
+            // Kiểm tra xem $_FILES có tồn tại
 
             $file = $_FILES['fileToUpload'];
             $file_path = $file['tmp_name'];
-            // Lấy thông tin về tệp tin tải lên và đường dẫn tạm thời
+            // Lấy thông tin về file tải lên và đường dẫn tạm thời
 
             if(file_exists($file_path)){
-                // Kiểm tra xem tệp tin tải lên có tồn tại
+                // Kiểm tra xem file tải lên có tồn tại
 
                 $file_content = file_get_contents($file_path);
-                // Đọc nội dung của tệp tin tải lên
+                // Đọc nội dung của file tải lên
 
                 echo "Dữ liệu từ tệp tin tải lên:<br>";
                 echo $file_content;
                 // Hiển thị nội dung của tệp tin
             } else {
                 echo "Không thể đọc tệp tin tải lên.";
-                // Thông báo nếu không thể đọc tệp tin tải lên
+                // Thông báo nếu không thể đọc file tải lên
             }
         }
     }
